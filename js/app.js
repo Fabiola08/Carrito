e0305dad6f02c475a1b076d5825a29c506248bf8
Fabiola08 <alejandrafabiolalopez58@gmail.com>
2022-10-03 11:08:10 -0500

    Recuperamos datos de objetos seleccionados

--- js/app.js
+++ js/app.js
@@ -4,12 +4,27 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarrito = document.querySelector('#vaciar-carrito');
 
+let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-        console.log("Presionaste el botón");
+        const curso = e.target.parentElement;
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('p.precio').textContent,
+            id:curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+        
+        agregarCarrito(infoCurso);
     }
+}
+
+const agregarCarrito = curso => {
+    listadoCarrito = [...listadoCarrito, curso]
+    console.log(listadoCarrito);
 }
 
 const cargarEventListener = () => {
