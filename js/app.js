12c047eb197ada40d88b6dd450823c5d1de117a1
Fabiola08 <alejandrafabiolalopez58@gmail.com>
2022-10-03 11:47:41 -0500

    Mostrando carrito en la pagina

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarrito = document.querySelector('#vaciar-carrito');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
 let listadoCarrito = [];
 
@@ -14,7 +14,7 @@ const agregarCurso = (e) => {
             imagen: curso.querySelector('img').src,
             nombre: curso.querySelector('h4').textContent,
             precio: curso.querySelector('p.precio').textContent,
-            id:curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
             cantidad: 1
         }
         
@@ -22,9 +22,39 @@ const agregarCurso = (e) => {
     }
 }
 
+
 const agregarCarrito = curso => {
     listadoCarrito = [...listadoCarrito, curso]
     console.log(listadoCarrito);
+    generaHTML();
+}
+
+const generaHTML = () => {
+    vaciarCarrito();
+    listadoCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        const cursoHTML = `
+        <td>
+            <img src="${curso.imagen}" width=100>
+        </td>
+        <td>
+            ${curso.nombre}
+        </td>
+        <td>
+            ${curso.precio}
+        </td>
+        <td>
+            ${curso.cantidad}
+        </td>
+        `;
+        row.innerHTML = cursoHTML;
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+const vaciarCarrito = () => {
+    
+
 }
 
 const cargarEventListener = () => {
